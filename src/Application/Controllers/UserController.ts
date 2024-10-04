5f0ce398ab4e7ce7db94ab9097e9c51bc2207dd3
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-10-04 09:37:30 -0300

    Controlador de usuario implementado

---
+++ src/Application/Controllers/UserController.ts
@@ -0,0 +1,65 @@
+import { Request, Response, NextFunction } from 'express';
+import IUserServices from '../../Domain/Interfaces/IUserServices';
+import CreateUserRequest from '../Requests/CreateUserRequest';
+import LoginRequest from '../Requests/LoginRequest';
+
+class UserController{
+    private userServices: IUserServices;
+    constructor(userServices: IUserServices){
+        this.userServices = userServices;
+    }
+    async createUser( req: Request, res: Response, next: NextFunction ): Promise<void>{
+        try{
+            const { username, email, password, role}: CreateUserRequest = req.body;
+            const createUserRequest = new CreateUserRequest(username, email, password, role);
+            const createdUser = await this.userServices.createUser(createUserRequest);
+            res.status(201).send(createdUser._id);
+        }
+        catch(error){
+            next(error);
+        }       
+    }
+    async deleteUser( req: Request, res: Response, next: NextFunction ): Promise<void>{
+        try{
+            const { userId } = req.body;
+            await this.userServices.deleteUser(userId);
+            res.status(200).send('Usuario eliminado con exito');
+        }
+        catch(error){
+            next(error);
+        }
+    }
+    async findUserById( req: Request, res: Response, next: NextFunction ): Promise<void>{
+        try{
+            const { userId } = req.body;
+            const retrievedUser = await this.userServices.findUserById(userId);
+            res.status(200).send(retrievedUser);
+        }
+        catch(error){
+            next(error);
+        }
+    }
+    async findUserByUsername( req: Request, res: Response, next: NextFunction ): Promise<void>{
+        try{
+            const { username } = req.body;
+            const retrievedUser = await this.userServices.findUserByUsername(username);
+            res.status(200).send(retrievedUser);
+        }
+        catch(error){
+            next(error);
+        }
+    }
+    async login( req: Request, res: Response, next: NextFunction ): Promise<void>{
+        try{
+            const { username, password }: LoginRequest = req.body;
+            const loginRequest = new LoginRequest(username, password);
+            const loginResponse = await this.userServices.login(loginRequest);
+            res.status(200).send(loginResponse);
+        }
+        catch(error){
+            next(error);
+        }
+    }
+}
+export default UserController;
+
