f26fb960048f0a0b7fa7b84651fcd79f968d5b32
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-10-04 09:41:52 -0300

    Modificacion de la solicitud de creacion de videojuego

--- src/Application/Requests/CreateVideogameRequest.ts
+++ src/Application/Requests/CreateVideogameRequest.ts
@@ -3,13 +3,11 @@ class CreateVideogameRequest{
     genre: Array<string>;
     description: string;
     imageUrl?: string;
-    averagePuntuation: Number;
     releaseDate: Date;
-    constructor(name: string, genre: Array<string>, description: string, averagePuntuation: Number, releaseDate: Date, imageUrl?: string) {
+    constructor(name: string, genre: Array<string>, description: string, releaseDate: Date, imageUrl?: string) {
         this.name = name;
         this.genre = genre;
         this.description = description;
-        this.averagePuntuation = averagePuntuation;
         this.releaseDate = releaseDate;
         this.imageUrl = imageUrl;
     }
--- src/Infrastructure/Models/VideogameModel.ts
+++ src/Infrastructure/Models/VideogameModel.ts
@@ -1,12 +1,11 @@
 import mongoose, { model, Schema } from "mongoose";
-import Videogame from "../../Domain/Entities/Videogame";
 import IVideogameDocument from "../Interfaces/IVideogameDocument";
 const videogameSchema: Schema<IVideogameDocument> = new mongoose.Schema({
     name: { type: String, required: true},
     genre: { type: [String], required: true},
     description: { type: String, required: true},
     imageUrl: { type: String },
-    averagePuntuation: { type: Number, required: true},
+    averagePuntuation: { type: Number, required: true, default: 0},
     releaseDate: { type: Date, required: true }
 })
 
