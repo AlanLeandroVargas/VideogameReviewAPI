da112a0c68974183a1219dfb6ade18ce989aee07
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-10-04 08:25:41 -0300

    Modificacion del modelo de reseña

--- src/Infrastructure/Models/ReviewModel.ts
+++ src/Infrastructure/Models/ReviewModel.ts
@@ -1,6 +1,6 @@
 import mongoose, { model, Schema } from "mongoose";
-import Review from "../../Domain/Entities/Review";
-const reviewSchema: Schema<Review> = new mongoose.Schema({
+import IReviewDocument from "../Interfaces/IReviewRepository";
+const reviewSchema: Schema<IReviewDocument> = new mongoose.Schema({
     author: { type: Schema.Types.ObjectId, ref: 'User', required: true},
     videogame: { type: Schema.Types.ObjectId, ref: 'Videogame', required: true},
     comment: { type: String, required: true},
@@ -8,6 +8,6 @@ const reviewSchema: Schema<Review> = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now },    
 })
 
-const reviewModel = model<Review>("Review", reviewSchema);
+const reviewModel = model<IReviewDocument>("Review", reviewSchema);
 
 export default reviewModel;
