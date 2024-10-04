357b9d6afe2d1028b06f7911e00b38da67a18a6d
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-10-04 09:29:54 -0300

    Excepcion de validaciones implementada

--- src/Application/Middleware/ErrorHandler.ts
+++ src/Application/Middleware/ErrorHandler.ts
@@ -3,26 +3,25 @@ import NotFoundException from "../Exceptions/NotFoundException";
 import { MongooseError } from "mongoose";
 import { MongoServerError } from 'mongodb';
 import UnauthorizedException from "../Exceptions/UnauthorizedException";
+import ValidationException from "../Exceptions/ValidationException";
 
-const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction): void => 
-    {
-        if(err instanceof NotFoundException)
-            {
-                res.status(404).json({error: err.message});                
-            }     
-        if(err instanceof UnauthorizedException)
-            {
-                res.status(401).json({error: err.message});
-            }
-        if(err instanceof MongooseError)
-            {
-                res.status(500).json("Database Error");
-            }
-        if(err instanceof MongoServerError)
-            {
-                res.status(500).json(err.message);
-            }
-        console.log(err.stack);
-        res.status(500).json({error: "Internal server error"});
-    };
+const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction): void => {
+    if (err instanceof NotFoundException) {
+        res.status(404).json({ error: err.message });
+    }
+    if (err instanceof ValidationException) {
+        res.status(401).json({ error: err.message });
+    }
+    if (err instanceof UnauthorizedException) {
+        res.status(401).json({ error: err.message });
+    }
+    if (err instanceof MongooseError) {
+        res.status(500).json("Database Error");
+    }
+    if (err instanceof MongoServerError) {
+        res.status(500).json(err.message);
+    }
+    console.log(err.stack);
+    res.status(500).json({ error: "Internal server error" });
+};
 export default errorHandler;
