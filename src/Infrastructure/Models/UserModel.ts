81e6106fbf7e39a54181256b5a514caf1f498b88
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-10-04 07:37:35 -0300

    Encriptacion implementada en el modelo de usuario

--- src/Infrastructure/Models/UserModel.ts
+++ src/Infrastructure/Models/UserModel.ts
@@ -1,13 +1,26 @@
 import mongoose, { model, Schema } from "mongoose";
-import User from "../../Domain/Entities/User";
+import IUserDocument from "../Interfaces/IUserDocument";
+import bcrypt from 'bcryptjs';
 
-const userSchema: Schema<User> = new mongoose.Schema({
+const userSchema: Schema<IUserDocument> = new mongoose.Schema({
     username: { type: String, required: true, unique: true},
     email: { type: String, required: true, unique: true},
     password: { type: String, required: true},
     role: { type: String, required: true}
 })
 
-const userModel = model<User>("User", userSchema);
+userSchema.pre<IUserDocument>('save', async function (next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+    next();
+});
+userSchema.methods.comparePassword = function (password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+};
+
+const userModel = model<IUserDocument>("User", userSchema);
 
 export default userModel;
